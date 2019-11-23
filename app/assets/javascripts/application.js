00aa49ad94b1f3a867d166779efd845ee6d29ec9
Shimol-Khan <se.shimolkhan@gmail.com>
2019-11-23 15:14:23 +0800

    Init And Done!

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require active_storage_drag_and_drop
+//= require turbolinks
+//= require_tree .
